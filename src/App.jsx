17b877701ff4bc0e99c304aa0a386c1995edbe37
kongjing-web <wanglong199005083031@gmail.com>
2026-06-23 10:04:44 +0800

    refactor: 调整项目目录结构，修复gitignore和vercel配置

--- src/App.jsx
+++ src/App.jsx
@@ -10,7 +10,7 @@ import {
   FaChartBar, FaTrashAlt, FaEdit, FaChevronDown,
   FaCoins, FaBell, FaBookOpen, FaHeadset, FaPaperPlane,
   FaArrowLeft, FaUsers, FaClipboardList, FaBullhorn, FaLock,
-  FaTelegram // 👈 补上这个漏网之鱼
+  FaTelegram 
 } from "react-icons/fa";
 
 import { useTranslation } from 'react-i18next';
